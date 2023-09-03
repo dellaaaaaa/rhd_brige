3972b100bfe955071dc2fc2a43fb0f5d70a34abd
Reyghita Hafizh <87755064+reyyghi@users.noreply.github.com>
2023-09-04 00:00:42 +0700

    Update client.lua

--- qb/client.lua
+++ qb/client.lua
@@ -1,1 +1,15 @@
+if not Shared.qb() then return end
 
+local QBCore = exports['qb-core']:GetCoreObject()
+
+Shared.PlayerData = function()
+  return QBCore.Functions.GetPlayerData()
+end
+
+Shared.GetPlayerJob = function()
+  return Shared.PlayerData().job
+end
+
+Shared.GetCitizenId = function()
+  return Shared.PlayerData().citizenid
+end
