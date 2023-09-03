7fb7cf7ddf6a9d16f1d171a544d09a26dc94bca6
dellaaaaaa <139941203+dellaaaaaa@users.noreply.github.com>
2023-09-03 23:42:34 +0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+game 'gta5'
+
+lua54 'yes'
+
+client_scripts {
+  'esx/client.lua',
+  'qb/client.lua'
+}
+
+shared_scripts {
+  'shared.lua'
+}
+
+server_scripts {
+  'esx/server.lua',
+  'qb/server.lua'
+}
